31610383e1b43343a58c2f089b2bd11e63e51d98
Balázs Szilágyi <bali723@gmail.com>
2013-02-15 16:29:21 +0100

    updated version number, date

--- Chrome/js/settings.js
+++ Chrome/js/settings.js
@@ -36,8 +36,8 @@ var cp = {
 			
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
-				html += '<p>Verzió: 3.0.5<br></p>';
-				html += '<p>Kiadás dátuma: 2012. 05. 24.</p>';
+				html += '<p>Verzió: 3.0.5.1<br></p>';
+				html += '<p>Kiadás dátuma: 2013. 02. 15.</p>';
 				html += '<p>Fejlesztő: JimMorrison723, Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 			html += '</div>';
