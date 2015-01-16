7120a7fb3cce03e8669e62eff39165e70ace2fe2
JimMorrison723 <bali723@gmail.com>
2015-01-16 12:58:14 +0100

    chrome new alpha release

--- Chrome/js/settings.js
+++ Chrome/js/settings.js
@@ -37,7 +37,7 @@ var cp = {
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
 				html += '<p>Verzió: 3.1.9.4 (Új felülethez készült alpha verzió)<br></p>';
-				html += '<p>Kiadás dátuma: 2015. 01. 14.</p>';
+				html += '<p>Kiadás dátuma: 2015. 01. 16.</p>';
 				html += '<p>Fejlesztő: JimMorrison723, Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 			html += '</div>';
