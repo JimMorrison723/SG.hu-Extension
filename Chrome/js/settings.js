b3dcf153f05bedd3886c819a252dd0b476c7aa9e
JimMorrison723 <bali723@gmail.com>
2015-06-04 11:32:10 +0200

    update version number

--- Chrome/js/settings.js
+++ Chrome/js/settings.js
@@ -36,8 +36,8 @@ var cp = {
 			
 			html += '<div class="settings_page">';
 				html += '<h3>SG Fórum+</h3>';
-				html += '<p>Verzió: 3.1.9.5 (Új felülethez készült alpha verzió)<br></p>';
-				html += '<p>Kiadás dátuma: 2015. 05. 04.</p>';
+				html += '<p>Verzió: 3.1.9.6 (Új felülethez készült alpha verzió)<br></p>';
+				html += '<p>Kiadás dátuma: 2015. 06. 04.</p>';
 				html += '<p>Fejlesztő: JimMorrison723, Gera János "dzsani" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 				html += '<p>Közreműködők: Viszt Péter "passatgt" <a href="http://visztpeter.me" target="_blank">http://visztpeter.me</a>, Krupa György "pyro" <a href="http://kreaturamedia.com" target="_blank">http://kreaturamedia.com</a></p>';
 			html += '</div>';
